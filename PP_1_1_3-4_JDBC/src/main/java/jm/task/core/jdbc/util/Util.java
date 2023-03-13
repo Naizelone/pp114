14fb736198bd08ad470285d5c32c1b998c043df4
Naizelone <125901953+Naizelone@users.noreply.github.com>
2023-03-13 17:40:52 +0300

    Add files via upload

---
+++ PP_1_1_3-4_JDBC/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,30 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/testdb";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+   private static Connection connection = null;
+
+
+    public Util(){
+        try {
+            connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+            if(!connection.isClosed()){
+                System.out.println("Подключение успешно");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Connection getConnection(){
+        return connection;
+    }
+}
